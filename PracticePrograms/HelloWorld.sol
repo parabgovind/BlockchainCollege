e95f91237a6fddb52ad53845d192fd3c594900e6
parabgovind <parabgovind0909@gmail.com>
2024-04-19 17:22:29 +0530

    understanding how to push from remix

---
+++ PracticePrograms/HelloWorld.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+contract HelloWorld{
+    function get() public pure returns (string memory){
+        return "Hello World";
+    }
+}

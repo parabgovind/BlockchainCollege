8a32a8fdec6e8a1ac827714f6006bfbe259c5f06
parabgovind <98024114+parabgovind@users.noreply.github.com>
2024-05-03 15:28:50 +0530

    Updated operators.sol

--- Practicals/operators.sol
+++ Practicals/operators.sol
@@ -1,31 +1,44 @@
 pragma solidity ^0.8.0;
+
 contract Operators{
-    uint public result;
-    bool public ans;
+    uint public result1;
+    uint public result2;
+    uint public result3;
+    uint public result4;
+    uint public result5;
+    
+    bool public ans1;
+    bool public ans2;
+    bool public ans3;
+    bool public ans4;
+    bool public ans5;
+    bool public ans6;
+    bool public ans7;
+    bool public ans8;
+    bool public ans9;
 
     constructor() {
         uint a = 10;
         uint b = 5; 
-        
 
-        //Arithmetic operators
-        result = a + b; // Addition
-        result = a - b; // Substraction
-        result = a * b; // Multiplication
-        result = a / b; // Division
-        result = a % b; // Modulus
+        // Arithmetic operators
+        result1 = a + b; // Addition
+        result2 = a - b; // Subtraction
+        result3 = a * b; // Multiplication
+        result4 = a / b; // Division
+        result5 = a % b; // Modulus
 
-        // //Comparison operators
-        ans = (a == b); //Equal to
-        ans = (a != b); // Not Equal to
-        ans= (a > b); //Greater than
-        ans = (a < b); //Less than
-        ans = (a >= b); //Greater than or equal to
-        ans = (a <= b); //Less than or Equal to
+        // Comparison operators
+        ans1 = (a == b); // Equal to
+        ans2 = (a != b); // Not Equal to
+        ans3 = (a > b);  // Greater than
+        ans4 = (a < b);  // Less than
+        ans5 = (a >= b); // Greater than or equal to
+        ans6 = (a <= b); // Less than or Equal to
 
-        // //Logical Operators
-        ans = (a > 0 && b > 0); //Logical AND
-        ans = (a > 0 || b > 0); //Logical OR
-        ans = !(a > 0); //Logical NOT
+        // Logical Operators
+        ans7 = (a > 0 && b > 0); // Logical AND
+        ans8 = (a > 0 || b > 0); // Logical OR
+        ans9 = !(a > 0);        // Logical NOT
     }
 }
